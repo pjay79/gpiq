e260a8741baa16838ceeabbf8397c337c0fe9900
pjay79 <praveen.jayarajan@gmail.com>
2016-11-21 21:54:13 +1100

    Basic landing page setup

--- scripts/gpiq_script.js
+++ scripts/gpiq_script.js
@@ -10,13 +10,36 @@ $(document).ready(function(){
   });
   // Hover touch
   $('body').bind('touchstart', function(){});
-  // Main header
+  // Main animation
+  //window.addEventListener("load");
+  //$(window).on("load", handler);
+ //$(window).on("load", function() {
   var main = new TimelineMax({paused:true});
-  main.set($('main'), {visibility: "visible"})
-      .from($('header img'), 1, {autoAlpha: 0, scale: 0, ease: Power4.easeOut})
-      .staggerFrom($('header h1 span'), 1, {autoAlpha: 0, ease: Power4.easeIn}, 0.25, 0)
-      .from($('header p'), 1, {autoAlpha: 0, y: 200, ease: Power1.easeInOut}, '-=0.8')
-      .add('headerIn')
-      .staggerFrom($('header p span'), 1, {autoAlpha: 0, ease: Power4.easeIn, repeat: -1, repeatDelay: 0.5}, 0.25, 'headerIn-=0.8');
+  main.set($('.main__content'), {visibility: "visible"})
+      .from($('.main__content img'), 0.3, {autoAlpha: 0, scale: 0, ease: Power4.easeOut})
+      .staggerFrom($('.main__content h1 span'), 0.3, {autoAlpha: 0, ease: Power4.easeIn}, 0.15, 0)
+      .from($('.main__content p'), 0.3, {autoAlpha: 0, ease: Power4.easeOut})
+      .from($('.main__content .call-to-action'), 0.3, {autoAlpha: 0, y: 50, ease: Power1.easeOut})
+      .from($('.main__social'), 0.3, {autoAlpha: 0, ease: Power4.easeOut})
+      .add('headerIn');
   main.play();
+  //});
+  // Form submission
+  $('form').submit(function() {
+        var message__confirmation = new TimelineMax();
+            message__confirmation.to($('#confirmation'), 0.5, {opacity: 1, ease: Expo.easeOut})
+                                 .to($('#confirmation'), 0.5, {autoAlpha: 0, ease: Expo.easeOut}, +0.5);
+        event.preventDefault();
+        var formData = $('form').serialize();
+        $.ajax({
+          type: 'POST',
+          url: $('form').attr('action'),
+          data: formData
+        });
+        $('#name').val('');
+        $('#email').val('');
+        $('#message').val('');
+        return false;
+    });
+
 });
