1f1315b415170c4fbc6df8b41905e25b61de3fa1
pjay79 <praveen.jayarajan@gmail.com>
2016-11-22 23:17:39 +1100

    Desktop and laptop set up

--- scripts/gpiq_script.js
+++ scripts/gpiq_script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+//$(document).ready(function(){
   // Set 100% viewport width/height to div's
   var fullscreen = function(){
     $('main').css({width: $(window).width(), height: $(window).height()});
@@ -11,19 +11,17 @@ $(document).ready(function(){
   // Hover touch
   $('body').bind('touchstart', function(){});
   // Main animation
-  //window.addEventListener("load");
-  //$(window).on("load", handler);
- //$(window).on("load", function() {
-  var main = new TimelineMax({paused:true});
+  $(window).on("load", function() {
+  var main = new TimelineMax({paused:true, delay: 0.5});
   main.set($('.main__content'), {visibility: "visible"})
       .from($('.main__content img'), 0.3, {autoAlpha: 0, scale: 0, ease: Power4.easeOut})
-      .staggerFrom($('.main__content h1 span'), 0.3, {autoAlpha: 0, ease: Power4.easeIn}, 0.15, 0)
+      .staggerFrom($('.main__content h1 span'), 0.3, {autoAlpha: 0, ease: Power4.easeOut}, 0.15, 0)
       .from($('.main__content p'), 0.3, {autoAlpha: 0, ease: Power4.easeOut})
-      .from($('.main__content .call-to-action'), 0.3, {autoAlpha: 0, y: 50, ease: Power1.easeOut})
-      .from($('.main__social'), 0.3, {autoAlpha: 0, ease: Power4.easeOut})
+      .from($('.main__content .call-to-action'), 0.25, {autoAlpha: 0, x: -150, ease: Bounce.easeOut})
+      .from($('.main__social'), 0.5, {autoAlpha: 0, scale: 0, y: 150, ease: Bounce.easeOut})
       .add('headerIn');
   main.play();
-  //});
+  });
   // Form submission
   $('form').submit(function() {
         var message__confirmation = new TimelineMax();
@@ -41,5 +39,4 @@ $(document).ready(function(){
         $('#message').val('');
         return false;
     });
-
-});
+//});
