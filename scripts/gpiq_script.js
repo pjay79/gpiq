1d6f6d01261ece48cb64edfeb9d8316a42ae23c7
pjay79 <praveen.jayarajan@gmail.com>
2017-01-02 21:59:29 +1100

    Add bourbon neat columns and remove bootstrap markup and styles

--- scripts/gpiq_script.js
+++ scripts/gpiq_script.js
@@ -1,3 +1,5 @@
+(function() {
+
 // @codekit-prepend "../bower_components/jquery/dist/jquery.min.js";
 // @codekit-prepend "../bower_components/gsap/src/minified/TweenMax.min.js";
 // @codekit-prepend "../bower_components/scrollmagic/scrollmagic/minified/ScrollMagic.min.js";
@@ -11,10 +13,21 @@ $(window).location.reload(true);
 });
 // Set 100% viewport width/height to div's
 var fullscreen = function(){
+  // Main
   $('main').css({width: $(window).width(), height: $(window).height()});
+  // Section 1
   $('.one').css({width: $(window).width(), height: $(window).height()});
+  // Section 2
   $('.two').css({width: $(window).width(), height: $(window).height()});
+  // Section 3
+  $('.left').css({height: $(window).height()});
+  $('.right').css({height: $(window).height()});
+  // Section 4
+  $('.left2').css({height: $(window).height() * 0.5});
+  $('.right2').css({height: $(window).height() * 0.5});
+  // Section 5
   $('.five').css({width: $(window).width(), height: $(window).height()});
+  // Portal
   $('.portal').css({width: $(window).width(), height: $(window).height()});
 };
 fullscreen();
@@ -39,49 +52,49 @@ main.play();
 // ScrollMagic scenes
 var controller = new $.ScrollMagic.Controller();
 // Trigger 1
-var scene = new $.ScrollMagic.Scene({triggerElement: "#trigger-one"});
+var scene1 = new $.ScrollMagic.Scene({triggerElement: "#trigger-one"});
 var section__animation_one = new TimelineMax();
 section__animation_one.from($('.one h1'), 1, {autoAlpha: 0, y: "+=100px", ease: Power4.easeOut})
-                   .staggerFrom($('.one p'), 1, {autoAlpha: 0, cycle:{x:[150,-150]}, ease: Power4.easeOut}, 0.25, 0.5);
-scene.setTween(section__animation_one);
-scene.addTo(controller);
+                      .staggerFrom($('.one p'), 1, {autoAlpha: 0, cycle:{x:[150,-150]}, ease: Power4.easeOut}, 0.25, 0.5);
+scene1.setTween(section__animation_one);
+scene1.addTo(controller);
 // Trigger 2
-var scene = new $.ScrollMagic.Scene({triggerElement: "#trigger-two"});
+var scene2 = new $.ScrollMagic.Scene({triggerElement: "#trigger-two"});
 var section__animation_two = new TimelineMax();
 section__animation_two.from($('.two h1'), 1, {autoAlpha: 0, y: "+=100px", ease: Power4.easeOut})
-                   .staggerFrom($('.two p'), 1, {autoAlpha: 0, cycle:{x:[150,-150]}, ease: Power4.easeOut}, 0.25, 0.5);
-scene.setTween(section__animation_two);
-scene.addTo(controller);
-//Trigger 4
-var scene = new $.ScrollMagic.Scene({triggerElement: "#trigger-three"});
+                      .staggerFrom($('.two p'), 1, {autoAlpha: 0, cycle:{x:[150,-150]}, ease: Power4.easeOut}, 0.25, 0.5);
+scene2.setTween(section__animation_two);
+scene2.addTo(controller);
+//Trigger 3
+var scene3 = new $.ScrollMagic.Scene({triggerElement: "#trigger-three"});
 var section__animation_three = new TimelineMax();
 section__animation_three.from($('.left h3'), 1, {autoAlpha: 0, y: "+=100px", ease: Power4.easeOut})
-                   .from($('.left img'), 1, {autoAlpha: 0, scale: 0, ease: Power4.easeOut}, 0.25)
-                   .from($('.right h3'), 1, {autoAlpha: 0, y: "+=100px", ease: Power4.easeOut}, 0.5)
-                   .from($('.right img'), 1, {autoAlpha: 0, scale: 0, ease: Power4.easeOut}, 0.75 );
-scene.setTween(section__animation_three);
-scene.addTo(controller);
+                        .from($('.left img'), 1, {autoAlpha: 0, scale: 0, ease: Power4.easeOut}, 0.25)
+                        .from($('.right h3'), 1, {autoAlpha: 0, y: "+=100px", ease: Power4.easeOut}, 0.5)
+                        .from($('.right img'), 1, {autoAlpha: 0, scale: 0, ease: Power4.easeOut}, 0.75 );
+scene3.setTween(section__animation_three);
+scene3.addTo(controller);
 //Trigger 4
-var scene = new $.ScrollMagic.Scene({triggerElement: "#trigger-four"});
+var scene4 = new $.ScrollMagic.Scene({triggerElement: "#trigger-four"});
 var section__animation_four = new TimelineMax();
 section__animation_four.from($('.four img'), 0.5, {autoAlpha: 0, scale: 0, ease: Power4.easeOut})
-                    .staggerFrom($('.four h3'), 1, {autoAlpha: 0, y: "+=100px", ease: Power4.easeOut}, 0.25, 0);
-scene.setTween(section__animation_four);
-scene.addTo(controller);
+                       .staggerFrom($('.four h3'), 1, {autoAlpha: 0, y: "+=100px", ease: Power4.easeOut}, 0.25, 0);
+scene4.setTween(section__animation_four);
+scene4.addTo(controller);
 //Portal
-var scene = new $.ScrollMagic.Scene({triggerElement: "#portal"});
+var portal = new $.ScrollMagic.Scene({triggerElement: "#portal"});
 var section__animation_portal = new TimelineMax();
 section__animation_portal.from($('.portal h1'), 1, {autoAlpha: 0, y: "+=100px", ease: Power4.easeOut})
                          .staggerFrom($('.portal h3'), 1, {autoAlpha: 0, y: "+=100px", ease: Power4.easeOut}, 0.25, 0.25);
-scene.setTween(section__animation_portal);
-scene.addTo(controller);
+portal.setTween(section__animation_portal);
+portal.addTo(controller);
 //Trigger 5
-var scene = new $.ScrollMagic.Scene({triggerElement: "#trigger-five"});
+var scene5 = new $.ScrollMagic.Scene({triggerElement: "#trigger-five"});
 var section__animation_five = new TimelineMax();
 section__animation_five.from($('.five h1'), 1, {autoAlpha: 0, y: "+=100px", ease: Power4.easeOut})
                        .staggerFrom($('.five img'), 1, {autoAlpha: 0, y: "+=100px", ease: Power4.easeOut}, 0.25, 0.25);
-scene.setTween(section__animation_five);
-scene.addTo(controller);
+scene5.setTween(section__animation_five);
+scene5.addTo(controller);
 // Form submission
 $('form').submit(function() {
       var message__confirmation = new TimelineMax();
@@ -104,3 +117,5 @@ $('.terms-and-privacy').click(function(){
  $('.terms-and-privacy__content').slideToggle();
  $("html, body").animate({ scrollTop: $(document).height() }, 1000);
 });
+
+})();
